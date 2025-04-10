e50cea2f660bcc3a50f84e0b66f8e2fb2e1398da
BBWonderis <martiska14lukas@gmail.com>
2025-04-10 16:35:40 +0200

    introduction section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import darkModeIcon from "./pictures/svg/dark_mode.svg";
 import Navbar from "./Navbar";
 import { useState } from "react";
 import "./style/dark-light-mode.css";
+import "./style/introduction.css";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -18,6 +19,27 @@ function App() {
           <img src={darkModeIcon} alt="dark_mode" className="dark-mode-icon" />
         </button>
       </header>
+      <section className="introduction">
+        <div className="objects">
+          <div className="photo">
+            <img src="/istockphoto-1335941248-612x612.jpg" alt="" />
+          </div>
+          <h2>Hi! I’m Lukáš Martiška! 👋🏻</h2>
+          <h1>
+            Computer science student at the Faculty of Informatics, Masaryk
+            University in Brno.
+          </h1>
+          <p>
+            I'm excited about building user-friendly and responsive web
+            interfaces, and I'm always open to new challenges and opportunities
+            to grow as a developer.
+          </p>
+          <div className="link-buttons">
+            <button className="github-button">my github →</button>
+            <button className="linkedln-button">my linkedln →</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
